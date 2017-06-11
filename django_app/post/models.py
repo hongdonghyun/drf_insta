4e69045ef6468d8efcee5fedc559b85a41482c3b
hongdonghyun <hong4367@gmail.com>
2017-06-11 15:53:45 +0900

    post model의 이미지,내용필드 MTM으로 comment,tag,post_like 연결

--- django_app/post/models.py
+++ django_app/post/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from member.models import User
 
 """
 member application 생성
@@ -11,8 +12,23 @@ member application 생성
 # Create your models here.
 
 class Post(models.Model):
-    user = models.ForeignKey(User)
-    pass
+    author = models.ForeignKey( #작성자 User참조
+        User,
+        on_delete=models.CASCADE
+    )
+    image = models.ImageField(null=True,blank=True) # Post image
+    content = models.TextField(null=True,blank=True) # Post content(내용)
+    comments = models.ManyToManyField(Comment)
+    tags = models.ManyToManyField(Tag)
+    post_like = models.ManyToManyField(
+        User,
+        through=PostLike,
+        related_name='post_like_by_User'
+    )
+
+    def __str__(self):
+        return '{}의 포스트 : {}'.foramt(self.author,self.content)
+
 
 
 class Comment(models.Model):
