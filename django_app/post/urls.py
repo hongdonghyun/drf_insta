b583168edc11549808cdf7be7791ee015e7dbaa1
hongdonghyun <hong4367@gmail.com>
2017-06-20 11:29:58 +0900

    삭제구현

--- django_app/post/urls.py
+++ django_app/post/urls.py
@@ -24,7 +24,9 @@ urlpatterns = [
     # post_create와 매칭
     url(r'^create/$', views.post_create, name='post_create'),
     url(r'^(?P<post_pk>\d+)/modify/$', views.post_modify, name='post_modify'),
-
+    url(r'^(?P<post_pk>\d+)/delete/$', views.post_delete, name='post_delete'),
+    url(r'^(?P<post_pk>\d+)/comment/create/$', views.comment_create, name='comment_create'),
     # 위쪽의 결과들과 매칭되지 않을 경우
     url(r'^.*/$', views.post_anyway, name='post_anyway'),
+
 ]
