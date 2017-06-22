9fddb47c56055f8d7c3c51a6f8b9cf4fcfe381bf
hongdonghyun <hong4367@gmail.com>
2017-06-22 21:04:36 +0900

    프로필 구현중

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -28,16 +28,79 @@ class User(AbstractUser):
     """
     # 이 User모델을 AUTH_USER_MODEL로 사용하도록 settings.py에 설정
     nickname = models.CharField(max_length=24, null=True, unique=True)
-    following = models.ManyToManyField(
+    relations = models.ManyToManyField(
         'self',
-        # through='Relation',
-    ) #자기자신에게 MTM필드 설정
+        through='Relation',
+        symmetrical=False,
+    )  # 자기자신에게 MTM필드 설정
+
+    # def follow(self, user):
+    #     # 해당 user를 follow하는 Relation을 생성한다.
+    #     # 이미 follow상태일 경우 행동x
+    #
+    #     if not isinstance(user, User):
+    #         raise ValueError('"user" argument must <User> class')
+    #
+    #     # Relation모델의 매니저 사용
+    #     Relation.objects.get_or_create(to_user=user, from_user=self)
+    #
+    #     # self로 주어진 User로부터 Relation의 from_user에 해당하는 RelatedManager를 사용
+    #     # self.follow_relations.get_or_create(to_user=user)
+    #
+    #     # user로 주어진 User로부터 Relation의 to_user에 해당하는 RelatedManager를 사용
+    #     # user.follower_relations.get_or_create(from_user=self)
+    #
+    # def unfollow(self, user):
+    #     # 반대
+    #     Relation.objects.filter(fromuser=self, to_user=user).delete()
+
+    def is_follow(self, user):
+        # 해당 user를 내가 follow하고 있는지 bool여부를 반환
+        return self.follow_relations.filter(to_user=user).exists()
+
+    def is_follower(self,user):
+        # 해당 user가 나를 follow하고 있는지 bool여부를 반환
+        return self.follow_reltations.filter(from_user=user).exists()
+
+    def follow_toggle(self, user):
+        relation, relation_created = self.follow_relations.get_or_create(to_user=user)
+
+        if not relation_created:
+            Relation.delete()
+
+    @property
+    def following(self):
+        # 내가 follow중인 User QuerySet
+        #return Relation.objects.filter(from_user=self)
+        relations = self.follow_relations.all()
+        return User.objects.filter(pk__in=relations.values('to_user'))
+
+    @property
+    def followers(self):
+        # '나를 follow중인 User QuerySet'
+        #return Relation.objects.filter(to_user=self)
+        relations = self.follower_relations.all()
+        return User.objects.filter(pk__in=relations.values('from_user'))
 
     def __str__(self):
         return self.nickname or self.username
         # return self.nickname if self.nickname else self.username
 
-# class Relation(models.Model):
-#     from_user = models.ForeignKey(User)
-#     to_user = models.ForeignKey(User)
-#     created_date = models.DateTimeField(auto_now_add=True)
+
+class Relation(models.Model):
+    from_user = models.ForeignKey(User, related_name='follow_relations')
+    #follow하는 사람 from_User
+    to_user = models.ForeignKey(User, related_name='follower_relations')
+    #follow 당하는사람 to_user
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'Relation from({}) to ({})'.format(
+            self.from_user,
+            self.to_user
+        )
+
+    class Meta:
+        unique_together = (
+            ('from_user', 'to_user'),
+        )  # 중복방지
