3398cd59a873f4a0e213589ce4271be83cfef730
hongdonghyun <hong4367@gmail.com>
2017-07-10 22:11:35 +0900

    게시글 올릴때 basic auth안하면 글 못씀

--- django_app/post/permissions.py
+++ django_app/post/permissions.py
@@ -2,9 +2,7 @@ from rest_framework import permissions
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
-    """
-    객체의 소유자에게만 쓰기를 허용하는 커스텀 권한
-    """
+
 
     def has_object_permission(self, request, view, obj):
         # 읽기 권한은 모두에게 허용하므로,
